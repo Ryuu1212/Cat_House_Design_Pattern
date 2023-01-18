50965fbfb34125407d83bfd5d4bbf75e38ee0156
chairielazizi <azizihr2000@gmail.com>
2023-01-18 16:36:54 +0800

    menu has worked to navigate to all panel, but the keylistener not work after switch to GamePanel

--- src/swingdemo/KeyHandler.java
+++ src/swingdemo/KeyHandler.java
@@ -21,15 +21,19 @@ public class KeyHandler implements KeyListener {
         
         if(code == KeyEvent.VK_W) {
             upPressed = true;
+            System.out.println("W pressed");
         }
         if(code == KeyEvent.VK_S) {
             downPressed = true;
+            System.out.println("S pressed");
         }
         if(code == KeyEvent.VK_A) {
             leftPressed = true;
+            System.out.println("A pressed");
         }
         if(code == KeyEvent.VK_D) {
             rightPressed = true;
+            System.out.println("D pressed");
         }
     }
 
--- src/swingdemo/MainFrame.java
+++ src/swingdemo/MainFrame.java
@@ -14,24 +14,27 @@ import javax.swing.event.MenuListener;
  *
  * @author amarabjamal
  */
-public class MainFrame extends JFrame {
-//    private static final JPanel cards = new JPanel(new CardLayout());
-    
+public class MainFrame extends JFrame {    
     CardLayout cardLayout;
     JPanel mainPanel;
     ControlPanel controlPanel;
     GamePanel gamePanel;
     DecoratorPanel decoratorPanel;
+    HomePanel homePanel;
     
     public MainFrame() {
+        KeyHandler keyHandler = new KeyHandler();
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
-        controlPanel = new ControlPanel();
+//        controlPanel = new ControlPanel();
         gamePanel = new GamePanel();
         decoratorPanel = new DecoratorPanel();
+        homePanel = new HomePanel();
+        controlPanel = new ControlPanel(mainPanel, cardLayout, gamePanel);
         mainPanel.add(controlPanel, "control");
         mainPanel.add(gamePanel, "game");
         mainPanel.add(decoratorPanel, "decorator");
+        mainPanel.add(homePanel, "change");
         
         //Creating the Frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit program if close-window button clicked
@@ -40,30 +43,27 @@ public class MainFrame extends JFrame {
         setVisible(true);    // "super" JFrame shows
         setLocationRelativeTo(null);
         
-//        ControlPanel control = new ControlPanel();
-//        control.setVisible(true);
-//        DecoratorPanel decorator = new DecoratorPanel();
-//        cards.add(control);
-//        cards.add(decorator);
         
         //Creating the MenuBar and adding components
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("FILE");
         JMenu m2 = new JMenu("Help");
         JMenu m3 = new JMenu("Menu");
-        mb.add(m1);
-        mb.add(m2);
+//        mb.add(m1);
+//        mb.add(m2);
         mb.add(m3);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m22);
+//        JMenuItem m11 = new JMenuItem("Open");
+//        JMenuItem m22 = new JMenuItem("Save as");
+//        m1.add(m11);
+//        m1.add(m22);
         
         MenuListener al1 = new MenuListener(){
             @Override
             public void menuSelected(MenuEvent e) {
-//                control.setVisible(true);
                 controlPanel.setVisible(true);
+                gamePanel.setVisible(false);
+                decoratorPanel.setVisible(false);
+                homePanel.setVisible(false);
                 System.out.println("Menu selected");
             }
 
@@ -76,81 +76,59 @@ public class MainFrame extends JFrame {
             public void menuCanceled(MenuEvent e) {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
-            };
+        };
         m3.addMenuListener(al1);
-//        m3.addActionListener(al1);
         
         //Creating the panel at bottom and adding components
         JPanel panel = new JPanel(); // the panel is not visible in output
+        panel.setBackground(Color.lightGray);
         panel.add(new JLabel("Hello, Player X!"));
         panel.add(new JButton("Start"));
         panel.add(new JButton("Pause"));
         
-        // GamePanel gamePanel = new GamePanel();
-//        cards.add(gamePanel);
-        
-        JToggleButton musicToggle = new JToggleButton("Toogle Music");
+        JToggleButton musicToggle = new JToggleButton("\uD83D\uDD0AToggle");
         panel.add(musicToggle);
         musicToggle.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("button was clicked!");
                 BackgroundSound sound =BackgroundSound.getInstance();
                 if(sound.getState()==1){
-                    gamePanel.stopMusic();
+                    System.out.println("Stop music!");
+//                    gamePanel.stopMusic();
+                    controlPanel.stopMusic();
                     sound.setState(2);
                 }
                 else if(sound.getState()==2){
-                    gamePanel.playMusic(0);
+                    System.out.println("Play music!");
+//                    gamePanel.playMusic(0);
+                    controlPanel.playMusic(0);
                     sound.setState(1);
         }
             }
         });
         
-        JButton goGame = new JButton("Go TO Game");
+        JButton goGame = new JButton("Back");
         panel.add(goGame);
         goGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cardLayout.show(mainPanel, "game");
+                cardLayout.show(mainPanel, "control");
+//                gamePanel.addKeyListener(keyHandler);
             }
         });
-        
-//        panel.add(new JButton(new AbstractAction("\u22b2Prev") {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                CardLayout cl = (CardLayout) cards.getLayout();
-//                cl.previous(cards);
-//            }
-//        }));
-//        panel.add(new JButton(new AbstractAction("Next\u22b3") {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                CardLayout cl = (CardLayout) cards.getLayout();
-//                cl.next(cards);
-//            }
-//        }));
-        HomePanel homePanel = new HomePanel();
-
-
         
         //Adding Components to the frame
         Container cp = getContentPane(); 
         cp.setLayout(new BorderLayout());
-//        cp.add(BorderLayout.CENTER,cards);
         cp.add(BorderLayout.NORTH,mb);
-//        // cp.add(BorderLayout.CENTER, gamePanel);
-        cp.add(BorderLayout.CENTER, homePanel);
         cp.add(BorderLayout.SOUTH, panel);
-//        cp.add(BorderLayout.WEST, control);
-//        cp.add(BorderLayout.CENTER, control);
-        
-
         add(mainPanel);
-        
-        // gamePanel.setUpGame();
-        // gamePanel.startGameThread();
+//        cp.add(BorderLayout.CENTER, gamePanel);
+//        cp.add(BorderLayout.CENTER, homePanel);
+//        cp.add(BorderLayout.WEST, controlPanel);
+//        cp.add(BorderLayout.CENTER, control);
+
+//        gamePanel.setUpGame();
+//        gamePanel.startGameThread();
     }
 }
